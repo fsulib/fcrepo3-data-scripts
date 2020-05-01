d8e633f889e838f4530b9a56efdf9e3f93a5f2b1
Bryan J. Brown <bjbrown@fsu.edu>
2020-05-01 19:39:17 -0400

    Sets up initial script

---
+++ create_filtered_copy.php
@@ -0,0 +1,5 @@
+#!/usr/bin/env php
+<?php
+
+require_once('variables.php');
+echo $output_home;
---
+++ variables.php
@@ -0,0 +1,5 @@
+<?php
+
+$fedora_home = '/usr/local/fedora';
+$namespaces = array('fsu', 'islandora');
+$output_home = '/data/fsudata1';
