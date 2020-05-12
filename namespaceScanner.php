6c09bfc2601879b9f3cf5fd5669ec6106ae172b9
Bryan J. Brown <bjbrown@fsu.edu>
2020-05-12 09:19:47 -0400

    Adds namespaceScanner.php script

---
+++ namespaceScanner.php
@@ -0,0 +1,29 @@
+#!/usr/bin/env php
+<?php
+require 'utilities.php';
+
+$counts = array('total' => 0);
+foreach ($namespaces as $namespace) {
+  $counts[$namespace] = 0;
+}
+
+$children = get_directory_contents($fedora_objectstore);
+foreach ($children as $child) {
+  $subchildren = get_directory_contents("{$fedora_objectstore}/{$child}");
+  foreach ($subchildren as $subchild) {
+    $uri_namespace = get_namespace_from_uri($subchild);
+    foreach ($namespaces as $namespace) {
+      if ($uri_namespace == $namespace) {
+        $counts[$namespace]++;
+      }      
+    } 
+    $counts['total']++;
+  }
+}
+
+echo "{$counts['total']} total objects in {$fedora_objectstore}.\n";
+foreach ($namespaces as $namespace) {
+  $percent = ($counts[$namespace] / $counts['total']) * 100;
+  $formatted_percent = number_format($percent, 1);
+  echo "{$counts[$namespace]} {$namespace} objects, {$formatted_percent}% of total objects.\n";
+}
--- variables.php
+++ variables.php
@@ -6,6 +6,8 @@ $log = '/root/fcrepo3-data-scripts/log.txt';
 $namespaces = array('fsu', 'islandora');
 
 $fedora_home = '/usr/local/fedora';
+$fedora_objectstore = '/data/objectStore';
+$fedora_datastreamstore = '/data/datastreamstore';
 
 $output_home = '/data/fsudata1/lost+found';
 
